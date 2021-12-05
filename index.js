838b63dd012967f6471a11b3d460344421a731b4
XtremMagic <95585031+XtremMagic@users.noreply.github.com>
2021-12-05 20:24:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const goToClient = require('./src/structures/goToClient');
 let client = new goToClient({
     prefix: '.'
 });
-client.login('OTE2NjQ3MzY5NjU3OTc0ODA2.YatMYw.cq-JfOKQIlVA4JthVRMTNXzU7-c')
+client.login('process.env.TOKEN')
